662ae38a4e0f4d4720147ed74e038dadcda26cf0
Ruud de Jong <rhjdjong@users.noreply.github.com>
2017-12-29 15:19:05 +0100

    Start from scratch

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ setup(
 #         'Programming Language :: Python :: 3',
 #         'Programming Language :: Python :: 3.2',
 #         'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.6',
     ],
 
     # What does your project relate to?
--- xdrlib2/__init__.py
+++
@@ -1,5 +0,0 @@
-__version__ = '0.0.1'
-
-from .xdr_base import *
-from .xdr_types import *
-from .xdr_tools import *
