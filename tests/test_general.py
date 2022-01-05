d7949bf2b7f95d72a76770e77ac092c92b31cd28
Ruud de Jong <rhjdjong@users.noreply.github.com>
2022-01-05 14:46:41 +0100

    Removed unnecessary files, added setuptools_scm for versioning

--- tests/test_general.py
+++ tests/test_general.py
@@ -2,9 +2,9 @@
 # This file is part of the xdrlib2 project which is released under the MIT license.
 # See https://github.com/rhjdjong/xdrlib2 for details.
 
-import pytest
 import sys
-import xdrlib2
+
 
 def test_import_xdrlib2():
+    import xdrlib2
     assert 'xdrlib2' in sys.modules
