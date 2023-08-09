7c6cf0e7663faa9b038f87b761ea3167ccbc1bb9
Ruud de Jong <rhjdjong@users.noreply.github.com>
2023-08-09 15:51:44 +0200

    Functionality of integer types added.

--- tests/core/test_integer.py
+++ tests/core/test_integer.py
@@ -4,24 +4,161 @@
 
 import pytest
 
-from xdrlib2.core.integer import Integer
+from xdrlib2.core.integer import Hyper, Integer, UnsignedHyper, UnsignedInteger, XdrInteger
 
 
-@pytest.mark.parametrize(
-    "value, encoded",
-    [
-        (0, b"\x00\x00\x00\x00"),
-        (324, b"\x00\x00\x01D"),
-        (2**31 - 1, b"\x7f\xff\xff\xff"),
-        (-527, b"\xff\xff\xfd\xf1"),
-        (-(2**31), b"\x80\x00\x00\x00"),
-    ],
-)
-def test_integer_encoding_and_decoding(value: int, encoded: bytes) -> None:
-    x = Integer(value)
-    assert bytes(x) == encoded
-    y = Integer._decode(encoded)
-    assert isinstance(y, Integer)
-    assert y == x
-    assert y == value
-    assert bytes(y) == encoded
+class TestInteger:
+    xdr_type = Integer
+    value_encoding = {
+        0: b"\x00\x00\x00\x00",
+        324: b"\x00\x00\x01D",
+        2**31 - 1: b"\x7f\xff\xff\xff",
+        -527: b"\xff\xff\xfd\xf1",
+        -(2**31): b"\x80\x00\x00\x00",
+    }
+
+    @pytest.mark.parametrize("value", [t for t in value_encoding])
+    def test_instantiation(self, value: int) -> None:
+        x = self.xdr_type(value)
+        assert isinstance(x, self.xdr_type)
+        assert x == value
+
+    @pytest.mark.parametrize("value, encoded", [(v, e) for v, e in value_encoding.items()])
+    def test_encoding_and_decoding(self, value: int, encoded: bytes) -> None:
+        x = self.xdr_type(value)
+        y = self.xdr_type.from_bytes(encoded)
+        assert type(y) is self.xdr_type
+        assert x == y
+        assert x.to_bytes() == encoded
+
+    @pytest.mark.parametrize("value", [min(v for v in value_encoding) - 1, max(v for v in value_encoding) + 1])
+    def test_invalid_value(self, value: int) -> None:
+        with pytest.raises(ValueError):
+            self.xdr_type(value)
+
+
+class TestUnsignedInteger:
+    xdr_type = UnsignedInteger
+    value_encoding = {
+        0: b"\x00\x00\x00\x00",
+        324: b"\x00\x00\x01D",
+        2**31 - 1: b"\x7f\xff\xff\xff",
+        2**31: b"\x80\x00\x00\x00",
+        0xDEADBEEF: b"\xde\xad\xbe\xef",
+        2**32 - 1: b"\xff\xff\xff\xff",
+    }
+
+    @pytest.mark.parametrize("value", [t for t in value_encoding])
+    def test_instantiation(self, value: int) -> None:
+        x = self.xdr_type(value)
+        assert isinstance(x, self.xdr_type)
+        assert x == value
+
+    @pytest.mark.parametrize("value, encoded", [(v, e) for v, e in value_encoding.items()])
+    def test_encoding_and_decoding(self, value: int, encoded: bytes) -> None:
+        x = self.xdr_type(value)
+        y = self.xdr_type.from_bytes(encoded)
+        assert type(y) is self.xdr_type
+        assert x == y
+        assert x.to_bytes() == encoded
+
+    @pytest.mark.parametrize("value", [min(v for v in value_encoding) - 1, max(v for v in value_encoding) + 1])
+    def test_invalid_value(self, value: int) -> None:
+        with pytest.raises(ValueError):
+            self.xdr_type(value)
+
+
+class TestHyper:
+    xdr_type = Hyper
+    value_encoding = {
+        0: b"\x00\x00\x00\x00\x00\x00\x00\x00",
+        324: b"\x00\x00\x00\x00\x00\x00\x01D",
+        2**31 - 1: b"\x00\x00\x00\x00\x7f\xff\xff\xff",
+        2**63 - 1: b"\x7f\xff\xff\xff\xff\xff\xff\xff",
+        -527: b"\xff\xff\xff\xff\xff\xff\xfd\xf1",
+        -(2**31): b"\xff\xff\xff\xff\x80\x00\x00\x00",
+        -(2**63): b"\x80\x00\x00\x00\x00\x00\x00\x00",
+    }
+
+    @pytest.mark.parametrize("value", [t for t in value_encoding])
+    def test_instantiation(self, value: int) -> None:
+        x = self.xdr_type(value)
+        assert isinstance(x, self.xdr_type)
+        assert x == value
+
+    @pytest.mark.parametrize("value, encoded", [(v, e) for v, e in value_encoding.items()])
+    def test_encoding_and_decoding(self, value: int, encoded: bytes) -> None:
+        x = self.xdr_type(value)
+        y = self.xdr_type.from_bytes(encoded)
+        assert type(y) is self.xdr_type
+        assert x == y
+        assert x.to_bytes() == encoded
+
+    @pytest.mark.parametrize("value", [min(v for v in value_encoding) - 1, max(v for v in value_encoding) + 1])
+    def test_invalid_value(self, value: int) -> None:
+        with pytest.raises(ValueError):
+            self.xdr_type(value)
+
+
+class TestUnsignedHyper:
+    xdr_type = UnsignedHyper
+    value_encoding = {
+        0: b"\x00\x00\x00\x00\x00\x00\x00\x00",
+        324: b"\x00\x00\x00\x00\x00\x00\x01D",
+        2**31 - 1: b"\x00\x00\x00\x00\x7f\xff\xff\xff",
+        2**31: b"\x00\x00\x00\x00\x80\x00\x00\x00",
+        0xDEADBEEF: b"\x00\x00\x00\x00\xde\xad\xbe\xef",
+        2**32 - 1: b"\x00\x00\x00\x00\xff\xff\xff\xff",
+        0xC0FFEECAFEFACADE: b"\xc0\xff\xee\xca\xfe\xfa\xca\xde",
+        2**64 - 1: b"\xff\xff\xff\xff\xff\xff\xff\xff",
+    }
+
+    @pytest.mark.parametrize("value", [t for t in value_encoding])
+    def test_instantiation(self, value: int) -> None:
+        x = self.xdr_type(value)
+        assert isinstance(x, self.xdr_type)
+        assert x == value
+
+    @pytest.mark.parametrize("value, encoded", [(v, e) for v, e in value_encoding.items()])
+    def test_encoding_and_decoding(self, value: int, encoded: bytes) -> None:
+        x = self.xdr_type(value)
+        y = self.xdr_type.from_bytes(encoded)
+        assert type(y) is self.xdr_type
+        assert x == y
+        assert x.to_bytes() == encoded
+
+    @pytest.mark.parametrize("value", [min(v for v in value_encoding) - 1, max(v for v in value_encoding) + 1])
+    def test_invalid_value(self, value: int) -> None:
+        with pytest.raises(ValueError):
+            self.xdr_type(value)
+
+
+class TestXdrInteger:
+    xdr_type = XdrInteger
+
+    def test_abstract_type_cannot_be_instantiated(self) -> None:
+        with pytest.raises(TypeError):
+            self.xdr_type(0)
+
+    def test_alias_for_abstract_type_not_allowed(self) -> None:
+        with pytest.raises(TypeError):
+            self.xdr_type.typedef("AnAlias")
+
+    def test_incomplete_typedef_not_allowed(self) -> None:
+        with pytest.raises(TypeError):
+            self.xdr_type.typedef("dummy", bits=16)
+        with pytest.raises(TypeError):
+            self.xdr_type.typedef("dummy", signed=False)
+
+    def test_custom_concrete_type(self) -> None:
+        short = self.xdr_type.typedef("short", bits=16, signed=False)
+        x = short(512)
+        e = x.to_bytes()
+        assert e == b"\x02\x00\x00\x00"
+        y = short.from_bytes(e)
+        assert type(y) is short
+        assert y == x
+
+    def test_invalid_typedef(self) -> None:
+        with pytest.raises(TypeError):
+            Integer.typedef("Subtype", signed=False)
--- xdrlib2/core/integer.py
+++ xdrlib2/core/integer.py
@@ -1,14 +1,77 @@
 # Copyright (c) 2023 Ruud de Jong
 # This file is part of the xdrlib2 project which is released under the MIT license.
 # See https://github.com/rhjdjong/xdrlib2 for details.
-from typing import Self
+from typing import Any, Literal, Self
+
+XDR_WORD_SIZE = 4
+XDR_BYTE_ORDER: Literal["little", "big"] = "big"
 
 
-class Integer(int):
-    def __bytes__(self) -> bytes:
-        return self.to_bytes(4, "big", signed=True)
+class XdrType:
+    _abstract = True
+
+    def __init_subclass__(cls, /, **kwargs: Any) -> None:
+        super().__init_subclass__(**kwargs)
+
+    def __new__(cls, *args: Any, **kwargs: Any) -> Self:
+        if cls._abstract:
+            raise TypeError(f"Cannot instantiate abstract base class {cls.__name__}")
+        return super().__new__(cls, *args, **kwargs)
+
+
+class XdrInteger(XdrType, int):
+    bits = 0
+    data_size = 0
+    hi = 0
+    lo = 0
+    padding = b""
+    signed = False
+    size = 0
+
+    def __init_subclass__(cls, /, bits: int | None = None, signed: bool | None = None, **kwargs: Any) -> None:
+        super().__init_subclass__(**kwargs)
+        if cls._abstract:
+            if bits is None or signed is None:
+                raise TypeError("Missing parameters in class definition")
+            cls.bits = bits
+            cls.signed = signed
+            cls.hi = 1 << (bits - int(signed))
+            cls.lo = -cls.hi if signed else 0
+            cls.data_size = 1 + (bits - 1) // 8
+            padding_size = (XDR_WORD_SIZE - (cls.data_size % XDR_WORD_SIZE)) % XDR_WORD_SIZE
+            cls.size = cls.data_size + padding_size
+            cls.padding = bytes(padding_size)
+            cls._abstract = False
+        else:
+            if bits is not None or signed is not None:
+                raise TypeError(f"Type alias for {cls.__name__} cannot redefine type.")
+
+    def __new__(cls, *args: Any) -> Self:
+        # Delegate argument validation to the superclass
+        instance = super().__new__(cls, *args)
+        if cls.lo <= instance < cls.hi:
+            return instance
+        raise ValueError(f"Invalid value {instance} for class {cls.__name__}")
+
+    def to_bytes(self) -> bytes:  # type: ignore[override]
+        return super().to_bytes(self.data_size, XDR_BYTE_ORDER, signed=self.signed) + self.padding
 
     @classmethod
-    def _decode(cls, bytestring: bytes) -> Self:
-        v = cls.from_bytes(bytestring, "big", signed=True)
+    def from_bytes(cls, bytestring: bytes) -> Self:  # type: ignore[override]
+        v = super().from_bytes(bytestring[: cls.data_size], XDR_BYTE_ORDER, signed=cls.signed)
         return cls(v)
+
+    @classmethod
+    def typedef(cls, name: str, **kwargs: Any) -> type[Self]:
+        return type(name, (cls,), {}, **kwargs)
+
+
+Int32 = XdrInteger.typedef("Int32", bits=32, signed=True)
+UInt32 = XdrInteger.typedef("UInt32", bits=32, signed=False)
+Int64 = XdrInteger.typedef("Int64", bits=64, signed=True)
+UInt64 = XdrInteger.typedef("UInt64", bits=64, signed=False)
+
+Integer = Int32.typedef("Integer")
+UnsignedInteger = UInt32.typedef("UnsignedInteger")
+Hyper = Int64.typedef("Hyper")
+UnsignedHyper = UInt64.typedef("UnsignedHyper")
